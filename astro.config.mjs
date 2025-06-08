a5449fe7926e10e88d3823dac0e6adbc39a618d9
Selim Ben Bouzid <benbouzid.selim@gmail.com>
2025-06-08 22:43:03 +0200

    All the pages but not the good Data

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+    server: {
+    watch: {
+      usePolling: true,
+      interval: 100,
+    },
+    host: true
+  }
+  }
+});
