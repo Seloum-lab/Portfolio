d68e8ec104644aeaaa402ca28783246ceb9bfd53
Selim Ben Bouzid <benbouzid.selim@gmail.com>
2025-06-17 00:12:02 +0200

    rectified the base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Seloum-lab.github.io',
-  base: 'Portfolio',
+  base: '/Portfolio/',
   trailingSlash: 'never',
   vite: {
     plugins: [tailwindcss()],
